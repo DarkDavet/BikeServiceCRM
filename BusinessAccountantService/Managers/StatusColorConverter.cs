a3fc59d4940b0a657e9d64f2c4a3492a8d8b3225
DarkDavet <darkdavet@gmail.com>
2026-03-06 14:07:45 +0300

    added status colors for combobox

---
+++ BusinessAccountantService/Managers/StatusColorConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace BusinessAccountantService.Managers
+{
+    public class StatusColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string status = value?.ToString();
+            return status switch
+            {
+                "Принят" => Brushes.LightGray,
+                "В работе" => Brushes.LightSkyBlue,
+                "Готов" => Brushes.LightGreen,
+                "Выдан" => Brushes.Gray,
+                _ => Brushes.White
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
